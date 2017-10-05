1d86059de8eedaae66217bb43ac475bf38c772dc
Petschko <peter@petschko.org>
2017-10-05 22:44:05 +0200

    Initiated Project

---
+++ src/org/petschko/updater/App.java
@@ -0,0 +1,22 @@
+package org.petschko.updater;
+
+/**
+ * Author: Peter Dragicevic [peter@petschko.org]
+ * Authors-Website: http://petschko.org/
+ * Date: 05.10.2017
+ * Time: 22:28
+ * Update: -
+ * Version: 0.0.1
+ *
+ * Notes: App Class
+ */
+public class App {
+	/**
+	 * Main-Class
+	 *
+	 * @param args - Optional Arguments from Command-Line
+	 */
+	public static void main(String[] args) {
+		// todo change
+	}
+}
