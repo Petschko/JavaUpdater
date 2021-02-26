3560fac1ae8f409b0d1f5727a34c329c9fae0888
Petschko <peter@petschko.org>
2021-02-26 15:06:36 +0100

    Added missing update stuff

--- src/org/petschko/updater/Updater.java
+++ src/org/petschko/updater/Updater.java
@@ -6,8 +6,14 @@ import org.petschko.lib.notification.ErrorWindow;
 import org.petschko.lib.notification.InfoWindow;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.util.Random;
 
 /**
  * Author: Peter Dragicevic [peter@petschko.org]
@@ -20,8 +26,11 @@ import java.net.URL;
  * Notes: Class Updater
  */
 class Updater {
+	private boolean hasErrors = false;
 	private URL downloadUrl;
 	private File targetJarFile;
+	private File tmpNewFile;
+	private File backupFile;
 
 	/**
 	 * Updater constructor
@@ -48,7 +57,13 @@ class Updater {
 			return;
 		}
 
+		// Do the Update
 		this.downloadFile();
+		this.replaceFile();
+
+		// Ends the Update routine
+		this.clear();
+		this.relaunch();
 	}
 
 	/**
@@ -86,8 +101,164 @@ class Updater {
 	/**
 	 * Downloads the new File
 	 */
-	void downloadFile() {
-		// todo
+	private void downloadFile() {
+		// Set Random name for new tmp File
+		Random random = new Random();
+		String tmpNewFileName;
+		do {
+			tmpNewFileName = "new_" + random.nextInt(1000) + "_" + App.targetJar;
+			this.tmpNewFile = new File(tmpNewFileName);
+		} while(this.tmpNewFile.exists());
+
+		System.out.println(cmdPrefix(Const.STATUS_INFO) + "Downloading Update-File...");
+		try {
+			ReadableByteChannel readableByteChannel = Channels.newChannel(this.downloadUrl.openStream());
+			FileOutputStream fileOutputStream = new FileOutputStream(tmpNewFileName);
+			fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+			fileOutputStream.close();
+		} catch(IOException e) {
+			this.showMessage("Could not download the Update-File...", Const.STATUS_ERROR, true, e);
+			this.hasErrors = true;
+		}
+		System.out.println(cmdPrefix(Const.STATUS_OK) + "Update-File saved to \"" + tmpNewFileName + "\"!");
+	}
+
+	/**
+	 * Replaces the Original JAR File with the new one
+	 */
+	private void replaceFile() {
+		if(this.hasErrors)
+			return;
+
+		System.out.println(cmdPrefix(Const.STATUS_INFO) + "Replacing Jar-File...");
+
+		// Check if tmp File exists
+		if(! this.tmpNewFile.exists() || this.tmpNewFile.isDirectory()) {
+			this.showMessage("Update-File does not exists...", Const.STATUS_ERROR, true);
+			this.hasErrors = true;
+			return;
+		}
+
+		// Backup original
+		String backupName = App.targetJar + ".bkp";
+		this.backupFile = new File(backupName);
+		if(this.backupFile.exists() && ! this.backupFile.isDirectory()) {
+			if(! this.backupFile.delete()) {
+				this.showMessage("Could not remove the old Backup-File...", Const.STATUS_ERROR, true);
+				this.hasErrors = true;
+				return;
+			}
+		}
+
+		try {
+			Files.copy(this.targetJarFile.toPath(), this.backupFile.toPath());
+		} catch(IOException ioException) {
+			this.showMessage("Could not backup the Original Jar-File...", Const.STATUS_ERROR, true);
+			this.hasErrors = true;
+			return;
+		}
+
+		// Delete original
+		if(! this.targetJarFile.delete()) {
+			this.showMessage("Could not remove the Original Jar-File...", Const.STATUS_ERROR, true);
+			this.hasErrors = true;
+			return;
+		}
+
+		// Copy the File
+		try {
+			Files.copy(this.tmpNewFile.toPath(), this.targetJarFile.toPath());
+		} catch(IOException ioException) {
+			this.showMessage("Could copy the new File to the Original Jar-File...", Const.STATUS_ERROR, true);
+			this.hasErrors = true;
+			return;
+		}
+
+		if(! targetJarFile.exists()) {
+			// Try to restore file if not exists
+			try {
+				Files.copy(this.backupFile.toPath(), this.targetJarFile.toPath());
+			} catch(IOException ioException) {
+				this.showMessage("Could not restore Original-File, please manually rename " + backupName + " to " + this.targetJarFile + "!", Const.STATUS_ERROR, true, ioException);
+				this.hasErrors = true;
+				return;
+			}
+
+			this.showMessage("Restored the Original Jar-File, since it could not copy the Update-File...", Const.STATUS_ERROR, true);
+			this.hasErrors = true;
+			return;
+		}
+
+		System.out.println(cmdPrefix(Const.STATUS_OK) + "Successfully replaced old Jar-File with new Jar!");
+	}
+
+	/**
+	 * Launches the original Program again if requested
+	 */
+	private void relaunch() {
+		if(this.hasErrors)
+			return;
+
+		System.out.println(cmdPrefix(Const.STATUS_OK) + "Update done!");
+		if(! App.relaunch)
+			return;
+
+		System.out.println(cmdPrefix(Const.STATUS_INFO) + "Relaunching \"" + App.targetJar + "\"...");
+
+		String[] run = {
+			"java",
+			"-jar",
+			"\"" + App.targetJar + "\""
+		};
+
+		// Add relaunch args
+		if(App.relaunchArgs != null) {
+			String[] tmp = new String[run.length + App.relaunchArgs.length];
+			int i;
+			for(i = 0; i < run.length; i++)
+				tmp[i] = run[i];
+
+			int n = i;
+			for(; i < tmp.length; i++)
+				tmp[i] = App.relaunchArgs[i - n];
+
+			run = tmp;
+		}
+
+		try {
+			Runtime.getRuntime().exec(run);
+		} catch (Exception e) {
+			this.showMessage("Could not restart the Program, please start it yourself!", Const.STATUS_WARNING, true, e);
+			this.hasErrors = true;
+			return;
+		}
+
+		System.out.println(cmdPrefix(Const.STATUS_OK) + "Done!");
+		System.exit(Const.STATUS_OK);
+	}
+
+	/**
+	 * Clears TMP Files etc
+	 */
+	private void clear() {
+		System.out.println(cmdPrefix(Const.STATUS_INFO) + "Clearing tmp Files...");
+
+		if(this.tmpNewFile != null) {
+			if(this.tmpNewFile.exists() && ! this.tmpNewFile.isDirectory()) {
+				if(! this.tmpNewFile.delete())
+					System.out.println(cmdPrefix(Const.STATUS_WARNING) + "Could not delete TMP-Update-File \"" + this.tmpNewFile.toString() + "\"");
+				else
+					System.out.println(cmdPrefix(Const.STATUS_OK) + "Deleted Update-File \"" + this.tmpNewFile.toString() + "\"!");
+			}
+		}
+		if(this.backupFile != null) {
+			if(this.backupFile.exists() && ! this.backupFile.isDirectory()) {
+				if(! this.backupFile.delete())
+					System.out.println(cmdPrefix(Const.STATUS_WARNING) + "Could not delete Backup Jar-File \"" + this.backupFile.toString() + "\"");
+				else
+					System.out.println(cmdPrefix(Const.STATUS_OK) + "Deleted Backup-File \"" + this.backupFile.toString() + "\"!");
+			}
+		}
 	}
 
 	/**
